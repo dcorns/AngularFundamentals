29f68a55018d92a3676ae273c3e459b3dfe10520
Dale <drcorns@hotmail.com>
2016-04-22 10:11:24 -0700

    Directives: Make directive declaration an element instead of an attribute

--- DemoApp/app/js/directives.js
+++ DemoApp/app/js/directives.js
@@ -1,6 +1,8 @@
 'use strict';
 eventsApp.directive('mySample', function($compile){
   return{
+    //default use of directive is by attribute use restrict to control how directive can be called. Below we make it an element
+    restrict: 'E',
     template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
   };
 });
