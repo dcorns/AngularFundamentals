2df770c7741f0805efcc7d5c96edad67ba9dd1a0
Dale <drcorns@hotmail.com>
2016-04-22 11:15:17 -0700

    add isolated scope to mySampe.js

--- DemoApp/app/js/directives/mySample.js
+++ DemoApp/app/js/directives/mySample.js
@@ -3,6 +3,9 @@ eventsApp.directive('mySample', function($compile){
   return{
     //default use of directive is by attribute use restrict to control how directive can be called. Below we make it an element
     restrict: 'AECM',//Attribute, Element, Class, Comment
-    template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
+    template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>",
+    scope:{
+      //Decoupling instances of the directive with isolated scope so all inputs and binding no longer equal the same data
+    }
   };
 });
