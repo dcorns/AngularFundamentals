add6325356d5e3967926c58e88b83e5da2bc06ee
Dale <drcorns@hotmail.com>
2016-04-22 10:46:00 -0700

    Directives: Declare directive as a class

--- DemoApp/app/js/directives.js
+++ DemoApp/app/js/directives.js
@@ -2,7 +2,7 @@
 eventsApp.directive('mySample', function($compile){
   return{
     //default use of directive is by attribute use restrict to control how directive can be called. Below we make it an element
-    restrict: 'E',
+    restrict: 'EC',
     template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
   };
 });
