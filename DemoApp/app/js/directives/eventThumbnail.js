6936aa0f3b7fbf0e5a0af7eb4808f2cdc22e858d
Dale <drcorns@hotmail.com>
2016-04-22 11:10:32 -0700

    Replace event thumbnail html with a view coupled thumbnail directive

---
+++ DemoApp/app/js/directives/eventThumbnail.js
@@ -0,0 +1,14 @@
+/**
+ * eventThumbnail
+ * Created by dcorns on 4/22/16
+ * Copyright © 2016 Dale Corns
+ */
+'use strict';
+
+eventsApp.directive('eventThumbnail', function () {
+  return{
+    restrict: 'E',
+    replace: true, //replace the directive tag with the html rather than putting the html inside the tag
+    templateUrl: 'templates/directives/eventThumbnail.html'
+  }
+});
