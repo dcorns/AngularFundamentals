6c6801d7bdb59786a9ead75ad2e915dc4802f8e0
Dale <drcorns@hotmail.com>
2016-04-22 10:04:26 -0700

    Directives: Replace link function for html output with a template property

--- DemoApp/app/js/directives.js
+++ DemoApp/app/js/directives.js
@@ -1,10 +1,6 @@
 'use strict';
 eventsApp.directive('mySample', function($compile){
   return{
-    //In the tutorial but did not work for me
-    link: function(scope, element, attrs, controller){
-      var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>";
-      angular.element(element).html($compile(markup)(scope));
-    }
+    template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
   };
 });
