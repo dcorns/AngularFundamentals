b74ff84bcd28b9fa31a01600da13b923f6543965
Dale <drcorns@hotmail.com>
2016-05-05 11:32:42 -0700

    DIRECTIVE: use compile service to fix input for repeated text feature

--- DemoApp/app/js/directives/repeatx.js
+++ DemoApp/app/js/directives/repeatx.js
@@ -4,13 +4,13 @@
  * Copyright © 2016 Dale Corns
  */
 'use strict';
-eventsApp.directive('repeatX', function(){
+eventsApp.directive('repeatX', function($compile){
   return{
     //using a link function. notice how it is recreating itself as well as the div content
-    //the input statement on the html only changes the original element and not the clones, the clones need to be compiled
+    //fix input with the compile service, used to manipulate the DOM prior to link function call
     link: function(scope, element, attributes, controller){
       for(var i = 0; i < Number(attributes.repeatX)-1; i++){
-        element.after(element.clone().attr('repeat-x', 0));
+        element.after($compile(element.clone().attr('repeat-x', 0))(scope));
       }
     }
   };
