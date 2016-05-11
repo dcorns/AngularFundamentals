8b0511551852bbaf5f98bad69361d661ee6ac56d
Dale <drcorns@hotmail.com>
2016-05-10 17:20:06 -0700

    TESTS: a less verbose approach to testing getEvent

--- DemoApp/test/unit/eventDataSpec.js
+++ DemoApp/test/unit/eventDataSpec.js
@@ -9,10 +9,18 @@ describe('eventData', function(){
   it('should make a get request to data/event/11 when getEvent is called and the ID is 11', inject(function(eventData, $httpBackend){
     $httpBackend.expectGET('data/event/11');
     $httpBackend.when('GET', 'data/event/11').respond({});
-    eventData.getEvent('11');
+    eventData.getEvent(11);
     $httpBackend.flush();
     $httpBackend.verifyNoOutstandingExpectation();
     $httpBackend.verifyNoOutstandingRequest();
   })
-  )
+  );
+
+  it('should return the correct data when getEvent is called', inject(function(eventData, $httpBackend){
+    $httpBackend.when('GET', 'data/event/11').respond({name: 'My Event'});
+    var event = eventData.getEvent(11);
+    $httpBackend.flush();
+    expect(event.name).toBe('My Event');
+  }));
+
 });
